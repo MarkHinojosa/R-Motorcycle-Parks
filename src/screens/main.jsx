45f642482ad0a02c63a3f9b74f4e6fcbf0839a83
mark <markhinojosa1212@gmail.com>
2019-10-21 00:51:38 -0500

    made changes

--- src/screens/main.jsx
+++ src/screens/main.jsx
@@ -15,20 +15,8 @@ const WrappedMap = withScriptjs(withGoogleMap(map));
 
 export default class Main extends Component {
   render() {
-    console.log(
-      "https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=AIzaSyCU39k6on-AXpbAnoMpPS8Qk2nvpghKPPk"
-    );
     return (
       <div style={{ height: "100vw", borderColor: "red", borderWidth: 10 }}>
-        {/* <WrappedMap
-          googleMapURL={
-            "https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=" +
-            process.env.REACT_APP_GOOGLE_KEY
-          }
-          loadingElement={<div style={{ height: `100%` }} />}
-          containerElement={<div style={{ height: `400px` }} />}
-          mapElement={<div style={{ height: `100%` }} />}
-        /> */}
         <Map />
       </div>
     );
--- src/screens/map/index.js
+++ src/screens/map/index.js
@@ -11,18 +11,19 @@ const Wrapper = styled.div`
 export default class Map extends Component {
   componentDidMount() {
     this.mymap = L.map("mymap", {
-      center: [15, 16],
+      center: [30, -100],
       zoom: 6,
       zoomControl: false
     });
     L.tileLayer(
-      "https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}",
+      "https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=" +
+        process.env.REACT_APP_MAPBOX_KEY,
       {
         attribution:
           'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
         maxZoom: 18,
         id: "mapbox.streets",
-        accessToken: "your.mapbox.access.token"
+        accessToken: process.env.REACT_APP_MAPBOX_KEY
       }
     ).addTo(this.mymap);
   }
