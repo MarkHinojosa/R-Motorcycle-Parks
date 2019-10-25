dab6cf7cd7bed308460c52b8b03265621ba5b26b
mark <markhinojosa1212@gmail.com>
2019-10-24 23:50:39 -0500

    added bootstrap

--- src/components/map/index.js
+++ src/components/map/index.js
@@ -2,19 +2,21 @@ import React, { Component } from "react";
 import { Map, TileLayer, Marker, Popup } from "react-leaflet";
 import L from "leaflet";
 import "leaflet/dist/leaflet.css";
-import styled from "styled-components";
 import "leaflet/dist/leaflet.css";
+import "../../styling/styles.css";
+import { Card, Button } from "react-bootstrap";
 
-const Wrapper = styled.div`
-  width: ${props => props.width};
-  height: ${props => props.height};
-`;
+const pinIcon = L.icon({
+  iconUrl: "https://unpkg.com/leaflet@1.5.1/dist/images/marker-icon-2x.png",
+  iconSize: [25, 41], // size of the icon
+  iconAnchor: [12.5, 41], // point of the icon which will correspond to marker's location
+  popupAnchor: [0, -41] // point from which the popup should open relative to the iconAnchor
+});
 
 export default class MapComponent extends Component {
   state = {
-    lat: 30,
-    lng: -100,
-    zoom: 8,
+    zoom: 3,
+    haveUsersLocation: false,
     userLocation: {
       lat: 0,
       lng: 0
@@ -22,43 +24,54 @@ export default class MapComponent extends Component {
   };
 
   async componentDidMount() {
-    navigator.geolocation.getCurrentPosition(position => {
-      this.setState(
-        {
+    navigator.geolocation.getCurrentPosition(
+      position => {
+        this.setState({
+          zoom: 14,
+          haveUsersLocation: true,
           userLocation: {
             lat: position.coords.latitude,
             lng: position.coords.longitude
           }
-        },
-        () => console.log(this.state)
-      );
-    });
+        });
+      },
+      err => console.log(err)
+    );
   }
 
-  render() {
-    var greenIcon = L.icon({
-      iconUrl: "https://unpkg.com/leaflet@1.5.1/dist/images/marker-icon-2x.png",
-      iconSize: [25, 41], // size of the icon
-      iconAnchor: [12.5, 41], // point of the icon which will correspond to marker's location
-      popupAnchor: [0, -41] // point from which the popup should open relative to the iconAnchor
-    });
+  displayMarker = () => {
+    return this.state.haveUsersLocation ? (
+      <Marker icon={pinIcon} position={this.state.userLocation}>
+        <Popup>
+          A pretty CSS3 popup. <br /> Easily customizable.
+        </Popup>
+      </Marker>
+    ) : null;
+  };
 
-    const position = [this.state.lat, this.state.lng];
-    const userLocation = [
-      this.state.userLocation.lat,
-      this.state.userLocation.lng
-    ];
+  render() {
     return (
-      <Map className="map" center={position} zoom={this.state.zoom}>
+      <Map
+        className="Map"
+        center={this.state.userLocation}
+        zoom={this.state.zoom}
+      >
         <TileLayer
           attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-        <Marker icon={greenIcon} position={userLocation}>
-          <Popup>
-            A pretty CSS3 popup. <br /> Easily customizable.
-          </Popup>
-        </Marker>
+        {this.displayMarker()}
+        <Card className="message-form">
+          <Card.Img variant="top" src="holder.js/100px180" />
+          <Card.Body>
+            <Card.Title>Card Title</Card.Title>
+            <Card.Text>
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </Card.Text>
+            <Button variant="primary">Go somewhere</Button>
+          </Card.Body>
+        </Card>
       </Map>
     );
   }
--- src/screens/main.jsx
+++ src/screens/main.jsx
@@ -1,11 +1,12 @@
 import React, { Component } from "react";
-import "./main.css";
+// import "./main.css";
 import MapComponent from "../components/map";
+import "../styling/styles.css";
 
 export default class main extends Component {
   render() {
     return (
-      <div>
+      <div className="main">
         <MapComponent />
       </div>
     );
