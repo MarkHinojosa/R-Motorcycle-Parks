b2165b8df4831a585810abdd5bd4ac899b7f6eaa
mark <markhinojosa1212@gmail.com>
2019-10-27 23:20:58 -0500

    added server code, missing db assignment

---
+++ server/app.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const app = express();
+const routes = require("./src/routes");
+
+const port = process.env.PORT || 3000;
+
+app.use("/api", routes);
+
+app.listen(port, () => console.log(`Listening on port ${port}...`));
