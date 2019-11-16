945375560e213800b53dbdb44b54ad23cc5f0a7d
mark <markhinojosa1212@gmail.com>
2019-11-15 21:43:07 -0600

    blah

--- src/screens/main.jsx
+++ src/screens/main.jsx
@@ -2,7 +2,6 @@ import React, { Component } from "react";
 // import "./main.css";
 import MapComponent from "../components/Map.js";
 import "../styling/styles.css";
-import Loading from "../components/LoadingScreen";
 
 export default class main extends Component {
   render() {
