03d9d0be85b9335399a768cbd89684557bf3a700
mark <markhinojosa1212@gmail.com>
2019-11-15 21:57:01 -0600

    testing url

--- src/components/FloatingCard.js
+++ src/components/FloatingCard.js
@@ -23,7 +23,7 @@ class FloatingCard extends Component {
 
   makePostToServer = () => {
     this.props.submittingData();
-    Axios.post(`${process.env.REACT_APP_API_URL}/apiI/submission`, {
+    Axios.post(`${process.env.API_URL}/api/submission`, {
       email: this.state.cardDetails.email,
       details: this.state.cardDetails.details,
       coordinates: [this.props.coordinates.lng, this.props.coordinates.lat],
